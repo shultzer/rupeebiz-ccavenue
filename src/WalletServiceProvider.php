42d847fe5994db769d0c9cfd3e2285b8d999f319
Sergei <skorohods@mail.ru>
2019-02-19 13:28:47 +0300

    first commit

--- src/WalletServiceProvider.php
+++ src/WalletServiceProvider.php
@@ -6,9 +6,9 @@ use Illuminate\Support\ServiceProvider;
 class WalletServiceProvider extends ServiceProvider
 {
     public function boot (  ) {
-        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
         $this->loadViewsFrom(__DIR__ . '/resources/views', 'shultz');
-        $this->publishes([__DIR__.'confic/wallet.php'=>config_path('wallet.php')]);
+        $this->publishes([__DIR__ . 'confic/wallet.php' =>config_path('wallet.php')]);
     }
 
     public function register (  ) {
